1a35728668ee848ce65e0a52cae0ed66bf2eacbe
lars0395 <larsfrauenrath@web.de>
2018-09-21 11:53:46 +0200

    frontend transclusion

--- Phase-2/k8s-bootcamp-assets/assets/js/bibliothek.js
+++ Phase-2/k8s-bootcamp-assets/assets/js/bibliothek.js
@@ -1,6 +1,41 @@
 const parser = new DOMParser();
 
 /*
+**  iq-embedded-link
+*/
+
+class IqEmbeddedLink extends HTMLElement {
+  async fetchDocument(uri) {
+    let response = await fetch(uri, {
+      credentials: "include"
+    });
+    if (response.ok) {
+      let htmlString = await response.text();
+      return parser.parseFromString(htmlString, "text/html");
+    }
+    return null;
+  }
+
+  async connectedCallback() {
+    let $a = this.querySelector("a");
+    let selector = this.getAttribute("data-target");
+    let $remoteDocument = await this.fetchDocument($a.href);
+
+    if ($remoteDocument) {
+      let $target = $remoteDocument.querySelector(selector);
+      if ($target) {
+        $a.setAttribute("hidden", "");
+        this.appendChild($target);
+      }
+    } else {
+      $a.classList.add("link--unreachable");
+    }
+  }
+}
+
+window.customElements.define("iq-embedded-link", IqEmbeddedLink);
+
+/*
 **  iq-form
 */
 
